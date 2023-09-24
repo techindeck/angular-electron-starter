eb35b7d6684f547d911c57b329846b04e2d7f723
spdocumentad <spdocumentad@gmail.com>
2023-09-24 13:03:25 +0530

    Initial Commit

---
+++ electron/preload.js
@@ -0,0 +1,23 @@
+// preload.js
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+// All the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })
+
+  // Expose a subset of Electron APIs to the renderer process
+contextBridge.exposeInMainWorld('api', {
+  openNewWindow: (url) => {
+    ipcRenderer.send('open-new-window', url);
+  }
+});
